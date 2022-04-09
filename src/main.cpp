37d5f84b6d888f3cfe8b00d85fe97cc6e9da11ca
Charles Rumford <charlesr@tallwireless.com>
2022-04-09 13:08:30 -0400

    Wo

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,51 @@
-#include "Adafruit_Protomatter.h"
-#include "Adafruit_GFX.h"
-#include "WiFiNINA.h"
+//#include "Adafruit_Protomatter.h"
+//#include "Adafruit_GFX.h"
+//#include "WiFiNINA.h"
+#include "Display.h"
 
-uint8_t rgbPins[]  = {7, 8, 9, 10, 11, 12};
-uint8_t addrPins[] = {17, 18, 19, 20};
-uint8_t clockPin   = 14;
-uint8_t latchPin   = 15;
-uint8_t oePin      = 16;
-
-Adafruit_Protomatter matrix(
-  256, 4, 1, rgbPins, 4, addrPins, clockPin, latchPin, oePin, false);
+uint16_t color565(uint8_t red, uint8_t green, uint8_t blue) {
+    return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
+}
 
 #define MATRIX_WIDTH  128
 #define MATRIX_HEIGHT 64
 
-GFXcanvas16 canvas(128,64);
+// WiFiClient espClient;
+// int wifiStatus = -1;
 
-WiFiClient espClient;
-int wifiStatus = -1;
-
-void wifiConnect(){
-  while (wifiStatus != WL_CONNECTED) {
-    Serial.print("Attempting to connect to WPA SSID: ");
-    Serial.println("LinuxCats");
-    Serial.print("STARTING STATUS: ");
-    Serial.println(wifiStatus);
-    wifiStatus = WiFi.begin("LinuxCats", "sophieisthcoolest");
-    Serial.print(" ENDING STATUS: ");
-    Serial.println(wifiStatus);
-    delay(10000);
-  }
-}
-
-void updateMatrix(void){
-    for( int w = 0; w < MATRIX_WIDTH; w++) {
-        for( int h = 0; h < MATRIX_HEIGHT/2; h++ ) {
-
-            matrix.drawPixel((MATRIX_WIDTH-w)-1, (MATRIX_HEIGHT/2)-h-1, canvas.getPixel(w,h));
-            // handle the lower part of the canvas
-            matrix.drawPixel(w+128,h,canvas.getPixel(w,h+MATRIX_HEIGHT/2));
-        }
-    }
-    matrix.show();
-}
+Display *canvas = NULL;
 
 void setup(void) {
-  Serial.begin(9600);
-  // Initialize matrix...
-  ProtomatterStatus status = matrix.begin();
-  Serial.print("Protomatter begin() status: ");
-  Serial.println((int)status);
-  if(status != PROTOMATTER_OK) {
-    for(;;);
-  }
-  String fv = WiFi.firmwareVersion();
-  if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
-    Serial.println("Please upgrade the firmware");
-  }
+  Serial.begin(9600) ;
+    pinMode(13, OUTPUT);
+  digitalWrite(13, HIGH);
+  delay(5000);
+  canvas = new Display(128, 64);
+
+  Serial.println("STARTING....");
 
 
-  canvas.setTextSize(3);
-  canvas.setCursor(0,24);
-  canvas.print("TESTING");
+  Serial.println("Write something?");
+ canvas->setTextSize(3);
+  canvas->setCursor(0,24);
+  canvas->print("TESTING");
+  Serial.println("update matrix");
 
-  updateMatrix();
+  canvas->updateMatrix();
+  Serial.println("updated matrix");
 }
-uint16_t colors[] = { matrix.color565(32,64,32), matrix.color565(64,0,32), matrix.color565(32,32,32) };
+uint16_t colors[] = { color565(,64,32), color565(64,0,32), color565(32,32,32) };
 int count = 0;
 void loop(void) {
+   digitalWrite(13, count++ % 2);
+
   Serial.print("Refresh FPS = ~");
-  Serial.println(matrix.getFrameCount());
-  canvas.setTextColor(colors[count++%3]);
-  canvas.setTextSize(3);
-  canvas.setCursor(0,24);
-  canvas.print("TESTING");
-  updateMatrix();
+  Serial.println(canvas->matrixFrameCount());
+  canvas->setTextColor(colors[count++%3]);
+  canvas->setTextSize(3);
+  canvas->setCursor(0,24);
+  canvas->print("TESTING");
+  canvas->updateMatrix();
 
   delay(1000);
 }
