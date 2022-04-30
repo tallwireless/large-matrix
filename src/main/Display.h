851d7537baafb22f9407882c95a5d7d539b6b5dd
Charles Rumford <charlesr@tallwireless.com>
2022-04-30 16:03:13 -0400

    Reformatting Code and adding color to print text

--- src/main/Display.h
+++ src/main/Display.h
@@ -1,36 +1,39 @@
 #pragma once
 
+#include "Widget.h"
 #include <Adafruit_GFX.h>
 #include <Adafruit_Protomatter.h>
-#include "Widget.h"
 
 class Display {
 
-    public:
-        Display(const int x, const int y);
-        void updateMatrix(void);
-        void clearBuffer(void) {
-            canvas->writeFillRect(0, 0, height, width, 0);
-        };
-        void clearSpace(const int, const int, const int, const int);
-        uint32_t matrixFrameCount(void) {
-            return matrix->getFrameCount();
-        };
-        void registerWidget(Widget*);
-        void updateWidgets(void);
-        void printText(int x, int y, String str) {
-            canvas->setTextSize(1);
-            canvas->setCursor(x, y);
-            canvas->print(str);
-            updateMatrix();
-        }
-    private:
-        GFXcanvas16 *canvas = NULL;
-        Adafruit_Protomatter *matrix = NULL;
-        Widget** widgets;
-        int widget_count = 0;
-        int widget_size = 1;
-        int width = -1;
-        int height = -1;
+   public:
+     Display(const int x, const int y);
+     void updateMatrix(void);
+     void clearBuffer(void) { canvas->writeFillRect(0, 0, height, width, 0); };
+     void clearSpace(const int, const int, const int, const int);
+     uint32_t matrixFrameCount(void) { return matrix->getFrameCount(); };
+     void registerWidget(Widget *);
+     void updateWidgets(void);
+     void printText(int x, int y, String str, uint16_t color = -1) {
+          if (color == -1) {
+               color = color565(32, 32, 32);
+          }
+          canvas->setTextColor(color);
+          canvas->setTextSize(1);
+          canvas->setCursor(x, y);
+          canvas->print(str);
+          updateMatrix();
+     }
+     static uint16_t color565(uint8_t red, uint8_t green, uint8_t blue) {
+          return ((red & 0xF8) << 8) | ((green & 0xFC) << 3) | (blue >> 3);
+     };
 
+   private:
+     GFXcanvas16 *canvas = NULL;
+     Adafruit_Protomatter *matrix = NULL;
+     Widget **widgets;
+     int widget_count = 0;
+     int widget_size = 1;
+     int width = -1;
+     int height = -1;
 };
